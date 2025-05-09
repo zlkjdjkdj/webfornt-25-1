5f6fc7d3c423ebc4ca8b3c212900cf86c646c571
zlkdjkdj <kil07201@naver.com>
2025-05-09 17:39:18 +0900

    1차 제작

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,34 @@
+
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Button  } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[text, setText] = useState("");
+  const[content, setContent] = useState("");
 
+  const onChange = (e) => {
+    setText(e.target.value);
+  }
+  const onClick  = () => {
+    setContent(text);
+    console.log(text)
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>My note</h1>
+    <div>
+    <input type = 'text' value = {text} onChange={onChange}></input>
+    <button onClick={onClick}>저장</button>
+    </div>
+    <p>input 입력값 : {text}</p>
+    <div>
+      <textarea value={content} onChange={(e)=>{
+        console.log(e.target.value)
+        setContent(e.target.value);
+      }}></textarea>
+    </div>
     </>
   )
 }
